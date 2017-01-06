681371c249b56f499243e48d16e16b87fdab4daf
maaxuy <maax.uy@gmail.com>
2017-01-06 15:24:07 -0300

    Ready to deploy

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
 
 	// The master channel
 	// The bot will announce the events to this channel. It won't listen to other channels.
-	master_channel: "265699201277493248",
+	master_channel: "266012052529741824",
 
 	// Events are considered "happening" for a margin of time, where users can see that the event
 	// is happening right now. During that time, the event is not considered expired yet.+
